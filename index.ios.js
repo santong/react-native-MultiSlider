2e5a935e9e04b01986343a3126b247f1b6e08911
Huszák Gábor <gabor.huszak@bigfish.hu>
2017-02-12 14:17:11 +0100

    Allow same thumbnail values

--- index.ios.js
+++ index.ios.js
@@ -13,14 +13,17 @@ class MultiSlider extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      leftValue: 0,
-      rightValue: 0.5,
+      s1leftValue: 2000,
+      s1rightValue: 2017,
+      s2leftValue: 2000,
+      s2rightValue: 2017,
     };
   }
 
   render() {
     return (
-      <View style = {{flex: 1, backgroundColor: 'white'}}>
+      <View style = {{flex: 1, backgroundColor: 'white', flexDirection: 'column'}}>
+          <Text style={{ padding: 10, marginTop: 40, fontWeight: 'bold'}}>Same thumb values not allowed</Text>
           <View style = {styles.container}>
             <MultiSliders
               ref = {(ms) => {this._ms = ms }}
@@ -29,22 +32,45 @@ class MultiSlider extends Component {
               leftThumbColor = {'red'}
               rightThumbColor = {'blue'}
               rangeColor = {'pink'}
-              leftValue = {this.state.leftValue}
-              rightValue = {this.state.rightValue}
-              onLeftValueChange = {(leftValue) => this.setState({leftValue})}
-              onRightValueChange = {(rightValue) => this.setState({rightValue})}
+              minValue = {2000}
+              maxValue = {2017}
+              step = {1}
+              min
+              leftValue = {this.state.s1leftValue}
+              rightValue = {this.state.s1rightValue}
+              onLeftValueChange = {(s1leftValue) => this.setState({s1leftValue})}
+              onRightValueChange = {(s1rightValue) => this.setState({s1rightValue})}
             />
+          </View>
+          <View style = {{flex: 1, backgroundColor: 'white', flexDirection: 'row', margin: 20}}>
+            <Text style={{ flex: 1, textAlign: 'left'}}>Left: {this.state.s1leftValue}</Text>
+            <Text style={{ flex: 1, textAlign: 'right'}}>Right: {this.state.s1rightValue}</Text>
+          </View>
+
+        <Text style={{ padding: 10, marginTop: 40, fontWeight: 'bold'}}>Same thumb values allowed</Text>
+        <View style = {styles.container}>
+          <MultiSliders
+              ref = {(ms) => {this._ms2 = ms }}
+              trackWidth = {300}
+              defaultTrackColor = {'#e3e3e3'}
+              leftThumbColor = {'red'}
+              rightThumbColor = {'blue'}
+              rangeColor = {'pink'}
+              minValue = {2000}
+              maxValue = {2017}
+              step = {1}
+              allowSameValues = {true}
+              leftValue = {this.state.s2leftValue}
+              rightValue = {this.state.s2rightValue}
+              onLeftValueChange = {(s2leftValue) => this.setState({s2leftValue})}
+              onRightValueChange = {(s2rightValue) => this.setState({s2rightValue})}
+          />
         </View>
-        <TouchableOpacity onPress = {() => this.onPress(true)}>
-          <View style = {styles.button}>
-            <Text>Click to disable</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity onPress = {() => this.onPress(false)}>
-          <View style = {styles.button}>
-            <Text>Click to able</Text>
-          </View>
-        </TouchableOpacity>
+        <View style = {{flex: 1, backgroundColor: 'white', flexDirection: 'row', margin: 20}}>
+          <Text style={{ flex: 1, textAlign: 'left'}}>Left: {this.state.s2leftValue}</Text>
+          <Text style={{ flex: 1, textAlign: 'right'}}>Right: {this.state.s2rightValue}</Text>
+        </View>
+
       </View>
     );
   }
